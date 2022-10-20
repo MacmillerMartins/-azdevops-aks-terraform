bc208469e7e8c37a43b79d5698256f7360554d9b
Macmiller <58778886+MacmillerMartins@users.noreply.github.com>
2022-10-20 08:38:33 -0300

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,9 +1,9 @@
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "azdevops-aks-agent"
+  name                = "aks-azdevops-aks-agent"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  dns_prefix = "azdevops-aks-agent"
+  dns_prefix = "aks-azdevops-aks-agent"
 
   default_node_pool {
     name       = "agentpool"
